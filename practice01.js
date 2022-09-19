2998a2bde6495ea8b28eb3253f84754515b9c916
AndressaFL <andressadefariaslazoti@gmail.com>
2022-09-18 22:00:57 -0500

    Changed the variable name

--- practice01.js
+++ practice01.js
@@ -1,13 +1,13 @@
 console.log("Hello world");
 
-let nameAdm = "Andressa";
-let ageAdm = 34;
-let favoritecolorAdm = "black";
+let nameFriend = "Andressa";
+let ageFriend = 34;
+let favoriteColorFriend = "black";
 
-const dobAdm = "Nov 2, 1986";
+const dobFriend = "Nov 2, 1986";
 
-console.log(nameAdm + " is " + ageAdm + " years old, she was born on " + dobAdm + " and her favourite color is " + favoritecolorAdm);
-console.log(`${nameAdm} is ${ageAdm} years old, she was born on ${dobAdm} and her favourite color is ${favoritecolorAdm}`);
+console.log(nameFriend + " is " + ageFriend + " years old, she was born on " + dobFriend + " and her favourite color is " + favoriteColorFriend);
+console.log(`${nameFriend} is ${ageFriend} years old, she was born on ${dobFriend} and her favourite color is ${favoriteColorFriend}`);
 
 let nameUser = "Rafael";
 let ageUser = 14;
@@ -15,20 +15,20 @@ let favoritecolorUser = "red";
 
 const dobUser = "May 28, 2008";
 
-if (ageAdm > ageUser) {
-    console.log(nameAdm + " is older than " + nameUser);
+if (ageFriend > ageUser) {
+    console.log(nameFriend + " is older than " + nameUser);
 } else
-    console.log(nameUser + " is older than " + nameAdm);
+    console.log(nameUser + " is older than " + nameFriend);
 
 let today = "Nov 2, 1986";
-if (today === dobAdm) {
-    ageAdm = ageAdm + 1;
-    console.log(nameAdm + " will be " + ageAdm + " years next year ");
+if (today === dobFriend) {
+    ageFriend = ageFriend + 1;
+    console.log(nameFriend + " will be " + ageFriend + " years next year ");
 }
 
-let totalofAges = ageAdm + ageUser;
-console.log("Sum of  ages of " + nameAdm + " and " + nameUser + " is " + totalofAges);
-console.log(`Sum of ages of ${nameAdm} and ${nameUser} is ${totalofAges}`);
+let totalofAges = ageFriend + ageUser;
+console.log("Sum of  ages of " + nameFriend + " and " + nameUser + " is " + totalofAges);
+console.log(`Sum of ages of ${nameFriend} and ${nameUser} is ${totalofAges}`);
 
 let number = 10;
 if (number % 2 === 0) {
@@ -47,7 +47,7 @@ if (a === 5) {
 console.log(a);
 console.log(b);
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i <= 10; i++) {
     let row = "8 * " + i + " = " + 8 * i;
     console.log(row);
 }
